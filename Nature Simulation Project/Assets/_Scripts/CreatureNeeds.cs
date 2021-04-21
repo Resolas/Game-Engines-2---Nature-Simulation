804295e8e3e77e3341652de789b7801ddae32b2e
Resolas <64108044+Resolas@users.noreply.github.com>
2021-04-21 03:38:37 +0100

    Hunter/Prey System
    - Added Fox Model
    - New Mode To LandAI To Hunt

--- Nature Simulation Project/Assets/_Scripts/CreatureNeeds.cs
+++ Nature Simulation Project/Assets/_Scripts/CreatureNeeds.cs
@@ -13,8 +13,26 @@ public class CreatureNeeds : MonoBehaviour      // Stats that creatures need to 
         curRest = Random.Range(maxRest/2,maxRest);
         curHealth = Random.Range(maxHealth/2,maxHealth);
 
+        int rng = Random.Range(0,2);
+        sex = sexArray[rng];
 
         StartCoroutine(PassiveConsumptionRate(5));
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (isPregnant && sex == "Female")
+        {
+            birthTime -= 1 * Time.deltaTime;
+
+            if (birthTime <= 0)
+            {
+                Reproduce();
+
+            }
+        }
 
     }
 
@@ -30,7 +48,39 @@ public class CreatureNeeds : MonoBehaviour      // Stats that creatures need to 
     public int curRest;
     public int curHealth;
 
+    public string mySpecies = "Animal";
+    public byte sizeClass = 0;
     
+    public int damage = 20;
+
+    [Header("Reproduction")]
+    public string sex;
+    private string[] sexArray = new string[]{"Male","Female"};
+    public bool isPregnant;
+ //   public bool isFertile;
+    public int minOffspring = 1;
+    public int maxOffspring = 3;
+    private int offspringTotal;
+    private float birthTime = 60f;
+    public GameObject myPrefab;
+
+    void Reproduce()
+    {
+        offspringTotal = Random.Range(minOffspring,maxOffspring);
+
+        for (int i = 0; i < offspringTotal; i++)
+        {
+          var newOffspring = Instantiate(myPrefab,transform.position,Quaternion.identity);
+
+
+        }
+
+        birthTime = Random.Range(30,120);
+        isPregnant = false;
+
+        Debug.Log(gameObject + " Gave Birth");
+    }
+
 
     IEnumerator PassiveConsumptionRate(int waitTime)
     {
--- Nature Simulation Project/Assets/_Scripts/LandAI.cs
+++ Nature Simulation Project/Assets/_Scripts/LandAI.cs
@@ -22,8 +22,12 @@ public class LandAI : MonoBehaviour
     [Header("AI Settings")]
     public float range = 100;
     public int time = 3;
+    
+    public bool isHunter = false;
+    
     [SerializeField]
     private bool canEat = false;
+    private bool canMate = false;
 
     IEnumerator RunAI(int waitTime)
     {
@@ -32,7 +36,16 @@ public class LandAI : MonoBehaviour
         {
             yield return new WaitForSeconds(waitTime);
 
-            RunAITree();
+            if (isHunter)
+            {
+                RunAITreeCarnivore();
+            }
+            else
+            {
+                RunAITreeHerbivore();
+            }
+
+            
 
           //  Debug.Log("TEST");
 
@@ -41,19 +54,19 @@ public class LandAI : MonoBehaviour
     }
 
 
-    void RunAITree()
+    void RunAITreeHerbivore()
     {
 
 
         if (getNeeds.isDead != true)
         {
 
-            // Wander
+            
 
             if (getNeeds.curFood < getNeeds.maxFood / 2)        // if less than half find food
             {
 
-                Collider[] getPlants = Physics.OverlapSphere(transform.position,range,1);
+                Collider[] getPlants = Physics.OverlapSphere(transform.position, range, 1);
                 float closest = 1000;
                 float currentTarget;
                 float getDist;
@@ -79,44 +92,200 @@ public class LandAI : MonoBehaviour
 
                         myAgent.destination = finalTarget.position;
                         canEat = true;
+                        canMate = false;
+                    }
+                    else    //      Keep Wandering
+                    {
+                        Wander();
+                    }
+
+                }
+
+
+            }
+            else if (getNeeds.curFood > 70 && getNeeds.sex == "Male") // Find Mate
+            {
+
+                Collider[] getMates = Physics.OverlapSphere(transform.position, range, 1);
+                float closest = 1000;
+                float currentTarget;
+                float getDist;
+                Transform finalTarget = null;
+
+                foreach (Collider myMate in getMates)
+                {
+
+                    if (myMate.CompareTag("Fauna") != true) continue;   // Skip if its not a plant (Trees Excluded)
+                    if (myMate.GetComponent<CreatureNeeds>().mySpecies != getNeeds.mySpecies) continue; // skip if not the same species
+                    if (myMate.GetComponent<CreatureNeeds>().isPregnant) continue;          // skip if its already pregnant
+                    if (myMate.GetComponent<CreatureNeeds>().sex != "Female") continue;
+
+                    getDist = Vector3.Distance(transform.position, myMate.transform.position);
+
+                    currentTarget = getDist;
+
+                    if (closest > currentTarget)    // gets the closest out of all
+                    {
+                        closest = currentTarget;
+                        finalTarget = myMate.transform;
+                    }
+
+                    if (finalTarget != null)        // sets destination to target if it exists
+                    {
+
+                        myAgent.destination = finalTarget.position;
+                        canMate = true;
                     }
                     else    //      stands still
                     {
-                        myAgent.destination = transform.position;
-                        canEat = false;
+                        Wander();
                     }
 
-                    Debug.Log(getPlants.Length);
-                    Debug.Log(finalTarget);
-
                 }
-                
+
+                if (finalTarget == null)
+                {
+                    Wander();
+                }
 
             }
             else   // Wander
             {
-                canEat = false;
-
-                Vector3 wanderPos = new Vector3(Random.Range(-20,20),0,Random.Range(-20,20));
-
-                myAgent.destination = wanderPos + transform.position;
+                Wander();
 
 
             }
 
+        }
 
+    }
+
+    void RunAITreeCarnivore()
+    {
+
+
+        if (getNeeds.isDead != true)
+        {
+
+            // Wander
+
+            if (getNeeds.curFood < getNeeds.maxFood / 2)        // if less than half find food
+            {
+
+                Collider[] getAnimals = Physics.OverlapSphere(transform.position, range, 1);
+                float closest = 1000;
+                float currentTarget;
+                float getDist;
+                Transform finalTarget = null;
+
+                foreach (Collider myAnimal in getAnimals)
+                {
+
+                    if (myAnimal.CompareTag("Fauna") != true) continue;   // Skip if its not a plant (Trees Excluded)
+                    if (myAnimal.GetComponent<CreatureNeeds>().mySpecies == getNeeds.mySpecies) continue;   // If same species skip
+                    if (myAnimal.GetComponent<CreatureNeeds>().sizeClass > getNeeds.sizeClass) continue;    // if larger skip
+
+                    getDist = Vector3.Distance(transform.position, myAnimal.transform.position);
+
+                    currentTarget = getDist;
+
+                    if (closest > currentTarget)    // gets the closest out of all
+                    {
+                        closest = currentTarget;
+                        finalTarget = myAnimal.transform;
+                    }
+
+                    if (finalTarget != null)        // sets destination to target if it exists
+                    {
+
+                        myAgent.destination = finalTarget.position;
+                        canEat = true;
+                    }
+                    else    //      stands still
+                    {
+                        //   myAgent.destination = transform.position;
+                        Wander();
+                    }
+
+                //    Debug.Log(getAnimals.Length);
+                //    Debug.Log(finalTarget);
+
+                }
+
+
+            }
+            else if (getNeeds.curFood > 70 && getNeeds.sex == "Male") // Find Mate
+            {
+
+                Collider[] getMates = Physics.OverlapSphere(transform.position, range, 1);
+                float closest = 1000;
+                float currentTarget;
+                float getDist;
+                Transform finalTarget = null;
+
+                foreach (Collider myMate in getMates)
+                {
+
+                    if (myMate.CompareTag("Fauna") != true) continue;   // Skip if its not a plant (Trees Excluded)
+                    if (myMate.GetComponent<CreatureNeeds>().mySpecies != getNeeds.mySpecies) continue; // skip if not the same species
+                    if (myMate.GetComponent<CreatureNeeds>().isPregnant) continue;          // skip if its already pregnant
+                    if (myMate.GetComponent<CreatureNeeds>().sex != "Female") continue;
+
+                    getDist = Vector3.Distance(transform.position, myMate.transform.position);
+
+                    currentTarget = getDist;
+
+                    if (closest > currentTarget)    // gets the closest out of all
+                    {
+                        closest = currentTarget;
+                        finalTarget = myMate.transform;
+                    }
+
+                    if (finalTarget != null)        // sets destination to target if it exists
+                    {
+
+                        myAgent.destination = finalTarget.position;
+                        canMate = true;
+                        canEat = false;
+                    }
+                    else    //      Keep Wandering :L
+                    {
+                        Wander();
+                    }
+
+                }
+
+                if (finalTarget == null)
+                {
+                    Wander();
+                }
+
+            }
+            else   // Wander
+            {
+
+                Wander();
+
+            }
 
         }
 
+    }
 
+    void Wander()
+    {
+        canEat = false;
+        canMate = false;
+        Vector3 wanderPos = new Vector3(Random.Range(-20, 20), 0, Random.Range(-20, 20));
 
+        myAgent.destination = wanderPos + transform.position;
     }
 
     private void OnCollisionStay(Collision collision)
     {
-        Debug.Log("TESTCOL");
+        //  Debug.Log("TESTCOL");
 
-        if (collision.collider.CompareTag("Flora") && canEat == true)
+        if (collision.collider.CompareTag("Flora") && canEat == true && isHunter != true)
         {
 
             var getFood = collision.collider.GetComponent<PlantGrowth>();
@@ -125,8 +294,31 @@ public class LandAI : MonoBehaviour
             Destroy(collision.gameObject);
 
         }
+        else if (collision.collider.CompareTag("Fauna") && canEat == true && isHunter == true)
+        {
+
+            var getFood = collision.collider.GetComponent<CreatureNeeds>();
+            getNeeds.curFood += (getFood.curFood / 2) + 10;
+            canEat = false;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.collider.CompareTag("Fauna") && canMate == true &&
+            collision.collider.GetComponent<CreatureNeeds>().mySpecies == getNeeds.mySpecies &&
+            collision.collider.GetComponent<CreatureNeeds>().isPregnant == false)
+        {
+            collision.collider.GetComponent<CreatureNeeds>().isPregnant = true;
+            getNeeds.curFood -= 30;
+            canMate = false;
+
+            Debug.Log(gameObject + " HAD MATED!");
+        }
 
     }
+
+    
+
+
 
     private void OnDrawGizmos()
     {
